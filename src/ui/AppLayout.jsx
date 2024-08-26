ad319a0dbbbd9030436cd1cad69ad84d379b39c7
Thomas S. Leasure <thomas.leasure@wv.gov>
2024-08-26 15:59:52 -0400

    fixed header/nav/main layout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,25 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
-import MainNav from "./MainNav";
+
 import Footer from "./Footer";
 
 function AppLayout() {
   return (
-    <div className="font-roboto  h-screen bg-slate-200">
-      <div className="bg-slate-50 grid grid-rows-[max-content_1fr] lg:mx-[10rem]">
-        <header>
-          <Header />
-        </header>
-        <nav>
-          <MainNav />
-        </nav>
-        <main className="mb-5">
-          <Outlet />
-        </main>
-        <footer>
-          <Footer />
-        </footer>
-      </div>
+    <div className="bg-slate-50 grid grid-rows-[max-content_1fr] h-screen font-roboto">
+      <header>
+        <Header />
+      </header>
+      <main className="mb-5">
+        <Outlet />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,16 +1,23 @@
+import MainNav from "./MainNav";
+
 function Header() {
   return (
-    <header className="grid grid-cols-3 h-[10rem] lg:h-[15rem]">
-      <div className="bg-green-700"></div>
-      <div className="bg-white flex justify-center items-center">
-        <img
-          src="example_pizza_logo.webp"
-          alt="Example Pizza Logo"
-          className="h-[10rem] lg:h-[15rem] w-auto"
-        />
-      </div>
-      <div className="bg-red-700"></div>
-    </header>
+    <>
+      <header className="grid grid-cols-3 h-[10rem] lg:h-[15rem]">
+        <div className="bg-green-700"></div>
+        <div className="bg-white flex justify-center items-center">
+          <img
+            src="example_pizza_logo.webp"
+            alt="Example Pizza Logo"
+            className="h-[10rem] lg:h-[15rem] w-auto"
+          />
+        </div>
+        <div className="bg-red-700"></div>
+      </header>
+      <nav>
+        <MainNav />
+      </nav>
+    </>
   );
 }
 
