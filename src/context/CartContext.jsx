5183eafd20c2ff2f2497d4a342ce71e5e2a8434b
Thomas S. Leasure <thomas.leasure@wv.gov>
2025-01-10 15:07:46 -0500

    disabled eslint react refresh on cart context

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -26,6 +26,7 @@ export const CartProvider = ({ children }) => {
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useCart = () => useContext(CartContext);
 
 export default CartContext;
