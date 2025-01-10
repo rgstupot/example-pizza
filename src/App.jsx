e86aeac2fd29229689533aa618791204b38270b6
Thomas S. Leasure <thomas.leasure@wv.gov>
2025-01-10 15:06:49 -0500

    created cart context

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,22 @@ import Main from "./pages/Main";
 import Menu from "./pages/Menu";
 import Coupons from "./pages/Coupons";
 import Rewards from "./pages/Rewards";
+import { CartProvider } from "./context/CartContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Main />} />
-          <Route path="menu" element={<Menu />} />
-          <Route path="coupons" element={<Coupons />} />
-          <Route path="rewards" element={<Rewards />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Main />} />
+            <Route path="menu" element={<Menu />} />
+            <Route path="coupons" element={<Coupons />} />
+            <Route path="rewards" element={<Rewards />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
